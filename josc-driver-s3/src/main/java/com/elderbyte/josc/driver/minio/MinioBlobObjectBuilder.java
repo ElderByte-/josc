5fda7ab5640ef96ff10ec69a2ffe0304830852aa
IsNull <p.buettiker@elderbyte.com>
2017-08-26 19:28:30 +0200

    improved null value handling, avoid NP on dates

--- josc-driver-s3/src/main/java/com/elderbyte/josc/driver/minio/MinioBlobObjectBuilder.java
+++ josc-driver-s3/src/main/java/com/elderbyte/josc/driver/minio/MinioBlobObjectBuilder.java
@@ -10,35 +10,60 @@ import io.minio.messages.Item;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
 
 class MinioBlobObjectBuilder {
 
     static BlobObject build(Item minioItem){
+
+        if(minioItem == null) throw new IllegalArgumentException("minioItem was NULL!");
+
         return new BlobObjectSimple(
                 minioItem.objectName(),
                 minioItem.objectSize(),
-                ZonedDateTime.ofInstant(minioItem.lastModified().toInstant(), ZoneId.systemDefault()),
+                toZonedDate(minioItem.lastModified()),
                 minioItem.etag(),
                 minioItem.isDir()
         );
     }
 
     static BlobObject build(ObjectStat stat){
+
+        if(stat == null) throw new IllegalArgumentException("stat was NULL!");
+
+
         return new BlobObjectSimple(
                 stat.name(),
                 stat.length(),
-                ZonedDateTime.ofInstant(stat.createdTime().toInstant(), ZoneId.systemDefault()),
+                toZonedDate(stat.createdTime()),
                 stat.etag(),
                 false
         );
     }
 
     static Bucket build(io.minio.messages.Bucket minioBucket){
+
+        if(minioBucket == null) throw new IllegalArgumentException("minioBucket was NULL!");
+
         return new BucketSimple(
                 minioBucket.name(),
-                LocalDateTime.ofInstant(minioBucket.creationDate().toInstant(), ZoneId.systemDefault()));
+                toLocalDate(minioBucket.creationDate()));
+    }
+
+    private static ZonedDateTime toZonedDate(Date date){
+        if(date != null){
+            return ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        }else{
+            return ZonedDateTime.now();
+        }
+    }
+
+    private static LocalDateTime toLocalDate(Date date){
+        if(date != null){
+            return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        }else{
+            return LocalDateTime.now();
+        }
     }
 }
