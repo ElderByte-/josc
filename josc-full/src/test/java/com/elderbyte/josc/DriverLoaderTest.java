7bcb55ae91d79f9478d278488215430e0ed48bb1
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-11-24 11:28:02 +0100

    updated driver load test

--- josc-full/src/test/java/com/elderbyte/josc/DriverLoaderTest.java
+++ josc-full/src/test/java/com/elderbyte/josc/DriverLoaderTest.java
@@ -9,7 +9,7 @@ public class DriverLoaderTest {
     @Test
     public void testServiceLoader(){
         JoscDriverManager driverManager = JoscDriverManager.getDefault();
-        Assert.assertEquals(3, driverManager.getDrivers().size());
+        Assert.assertEquals(4, driverManager.getDrivers().size());
     }
 
 }
