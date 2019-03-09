e88d8cc62babf8f5a4b61ed9f7c0464656b4bc4c
IsNull <p.buettiker@elderbyte.com>
2019-03-09 17:52:55 +0100

    driver config clean up

--- josc-driver-s3/src/main/java/com/elderbyte/josc/driver/aws/JoscDriverAwsS3.java
+++ josc-driver-s3/src/main/java/com/elderbyte/josc/driver/aws/JoscDriverAwsS3.java
@@ -20,48 +20,30 @@ public class JoscDriverAwsS3 implements JoscDriver {
     public ObjectStoreClient openConnection(String host, JoscConnectionProperties properties) throws ObjectStoreConnectionException {
         try {
 
-
-            MinioClient minioClient = new MinioClient(
-                    host,
+            var credentials = AwsBasicCredentials.create(
                     properties.getRequiredProperty("user"),
                     properties.getRequiredProperty("pass")
             );
 
+            var minioClient = new MinioClient(
+                    host,
+                    credentials.accessKeyId(),
+                    credentials.secretAccessKey()
+            );
 
-            S3Client s3Client = S3Client.builder()
-                    .region(Region.US_EAST_1)
+            var s3Client = S3Client.builder()
+                    .region(
+                            properties.getProperty("region")
+                                .map(Region::of)
+                                .orElse(Region.US_EAST_1)
+                    )
                     .endpointOverride(new URI(host))
-                    .credentialsProvider(StaticCredentialsProvider.create(
-                            AwsBasicCredentials.create(
-                                    properties.getRequiredProperty("user"),
-                                    properties.getRequiredProperty("pass")
-                            )
-                    ))
+                    .credentialsProvider(StaticCredentialsProvider.create(credentials))
                     .serviceConfiguration(S3Configuration.builder()
                             .pathStyleAccessEnabled(true)
                             .build())
                     .build();
 
-
-            /*
-
-            ClientConfiguration clientConfiguration = new ClientConfiguration();
-            clientConfiguration.setSignerOverride("AWSS3V4SignerType");
-
-            AmazonS3 awsS3 = AmazonS3ClientBuilder
-                    .standard()
-                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(host, "us-east-1"))
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
-                            properties.getRequiredProperty("user"),
-                            properties.getRequiredProperty("pass")
-                    )))
-                    .withChunkedEncodingDisabled(true)
-                    .withClientConfiguration(clientConfiguration)
-                    .withPathStyleAccessEnabled(true)
-                    .build();
-
-
-            */
             return new AwsS3ObjectStoreClient(s3Client, minioClient);
         }catch (Exception e){
             throw new ObjectStoreConnectionException("Failed to open connection to minio/S3 object store!", e);
@@ -70,11 +52,11 @@ public class JoscDriverAwsS3 implements JoscDriver {
 
     @Override
     public boolean supports(String protocol, String host, JoscConnectionProperties properties) {
-        return "s3".equalsIgnoreCase(protocol) || "aws".equalsIgnoreCase(protocol);
+        return "s3".equalsIgnoreCase(protocol) || "minio".equalsIgnoreCase(protocol);
     }
 
     @Override
     public String toString() {
-        return "JoscDriverAwsS3{ protocols: [s3, aws] }";
+        return "JoscDriverAwsS3{ protocols: [s3, minio] }";
     }
 }
